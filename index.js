3789c2a059725858d88963c3ebc67f04f7dbdf53
BryanCabrera24 <147127942+dev-bryancabrera@users.noreply.github.com>
2025-03-27 08:53:25 -0500

    modificacion para que al levantar la aplicacion escuche en ipv4

--- index.js
+++ index.js
@@ -970,7 +970,7 @@ const startServer = async () => {
       }
     }
 
-    server.listen(port, () => {
+    server.listen(port, "0.0.0.0", () => {
       console.log("Server Run Port : " + port);
     });
   } catch (error) {
