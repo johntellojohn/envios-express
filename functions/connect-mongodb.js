fbc009c1c1b51f62bb5ccf1bba998ceae08c6f0a
kellypaltin <kelly.paltin23@gmail.com>
2025-07-10 11:18:37 -0500

    Ajustes para el entorno de produccion

--- functions/connect-mongodb.js
+++ functions/connect-mongodb.js
@@ -13,7 +13,7 @@ const connectToMongoDB = async () => {
     await mongoClient.connect();
     console.log("Conectado a MongoDB correctamente");
 
-    const db = mongoClient.db("pruebas_whatsapp");
+    const db = mongoClient.db("ariana_crm");
 
     return db;
   } catch (error) {
